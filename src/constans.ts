1e0dec0e98f80069ad2aff446b822aa1622eb5df
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-29 18:14:30 +0600

    added ApiQuoteType;

--- src/constans.ts
+++ src/constans.ts
@@ -1,7 +1,15 @@
-export const categories = [
+import {CategoryType} from './types';
+
+export const categories: CategoryType[] = [
   {title: 'Star Wars', id: 'star-wars'},
   {title: 'Famous people', id: 'famous-people'},
   {title: 'Saying', id: 'saying'},
   {title: 'Humour', id: 'humour'},
   {title: 'Motivational', id: 'motivational'},
 ];
+
+export const getCategoryTitle = (categories: CategoryType[], id: string | undefined) => {
+  let currentCategory = categories.find(obj => obj.id === id);
+
+  return currentCategory ? currentCategory.title : 'All';
+};
