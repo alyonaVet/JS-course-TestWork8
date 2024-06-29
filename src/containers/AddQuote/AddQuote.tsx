3a29c62c9b8f29a3f611eb71ee78436e3a516eef
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-29 13:49:29 +0600

    added request post to database;

--- src/containers/AddQuote/AddQuote.tsx
+++ src/containers/AddQuote/AddQuote.tsx
@@ -1,6 +1,8 @@
 import React, {ChangeEvent, useState} from 'react';
 import {QuoteType} from '../../types';
 import AddQuoteForm from '../../components/AddQuoteForm/AddQuoteForm';
+import axiosApi from '../../axiosApi';
+import {useNavigate} from 'react-router-dom';
 
 const AddQuote = () => {
   const [quote, setQuote] = useState<QuoteType>({
@@ -8,6 +10,8 @@ const AddQuote = () => {
     author: '',
     quote: '',
   });
+
+  const navigate = useNavigate();
 
   const onFieldChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
     const { name, value } = event.target;
@@ -20,7 +24,16 @@ const AddQuote = () => {
 
   const onFormSubmit = async (event: React.FormEvent)=> {
     event.preventDefault();
-  }
+
+    const newQuote = {
+      quote,
+    };
+    try {
+      await axiosApi.post('/quotes.json', newQuote);
+    }finally {
+      navigate('/');
+    }
+  };
 
   return (
     <div className="container mt-3">
