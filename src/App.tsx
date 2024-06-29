d5ceec0d177ae5660e2913d889978e0573c2c025
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-29 18:16:56 +0600

    added header; added all routes to App;

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import './App.css';
+import Toolbar from './components/Toolbar/Toolbar';
+import AddQuote from './containers/AddQuote/AddQuote';
+import {Route, Routes} from 'react-router-dom';
+import MainPage from './containers/MainPage/MainPage';
+import EditQuote from './containers/EditQuote/EditQuote';
 
 const App = () => {
+
   return (
     <>
-        Hello, world!
+      <header>
+        <Toolbar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/add-quote" element={<AddQuote />} />
+          <Route path="/quotes/:category" element={<MainPage />} />
+          <Route path="/quotes/:id/edit" element={<EditQuote />} />
+        </Routes>
+      </main>
     </>
   )
 };
